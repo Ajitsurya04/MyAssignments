f88c1cc0fc76dfab0df2b0e5587dc6b9ed7bac23
Ajitsurya04 <ajithsurya199704@gmail.com>
2024-10-13 14:36:23 +0530

    Add files via upload
    Day 1

---
+++ day1/Chrome.java
@@ -0,0 +1,18 @@
+package Assignment1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float versionoftype = 91.0f;
+		String developeroftype = "Google"; 
+		boolean Betaoftype = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println("Chrome version is "+versionoftype);
+		System.out.println("Browser name is "+developeroftype);
+		System.out.println("Chrome value is "+Betaoftype);
+		System.out.println("Chrome relase year is "+releaseYear);
+		System.out.println("Chrome Shortcutkey is "+shortcutKey);
+	}
+}
